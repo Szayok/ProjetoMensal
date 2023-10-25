a563a78e5b7982c0da5e0fac114a0e31fb3a9bfd
Joshua Binotto <133135094+kisalto@users.noreply.github.com>
2023-10-25 14:55:52 -0300

    Update addCarros.js

--- js/addCarros.js
+++ js/addCarros.js
@@ -1,10 +1,17 @@
-const modal = document.getElementById("myModal");
-const fechar = document.getElementById("fechar-modal");
+let modal = document.getElementById("myModal");
+let fechar = document.getElementById("fechar-modal");
+
+var pos = 0;
 
 document.addEventListener("DOMContentLoaded", function() {
     fechar.addEventListener("click", function() {
         modal.style.display = "none";
     });
+    if (localStorage.length != 0) {
+        for (let i = 0; i < localStorage.length;i++) {
+            carrosCadastrados();
+        }
+    }
 });
 
 function abrirModal() {
@@ -16,26 +23,32 @@ function adicionarCarro() {
     let ano = document.getElementById("ano").value;
     let preco = document.getElementById("preco").value;
 
-    let veiculos = document.getElementById("veiculos-grid");
-
+    
+    pos++;
+    
     // <section class="veiculo">
     //         <img src="/img/carro 2.jpeg" alt="Carro 2">
     //         <h2>Carro 2</h2>
     //         <p>Ano: 2022</p>
     //         <p>Preço: R$ 45.000,00</p>
     // </section>
-
+    
+    let veiculos = document.getElementById("veiculos-grid");
     let sessao = document.createElement("section");
     let imagem = document.createElement("img");
     let titulo = document.createElement("h2");
     let desc1 = document.createElement("p");
     let desc2 = document.createElement("p");
 
-    titulo.innerHTML = nome;
-    imagem.src = "/img/carro 2.jpeg";
     sessao.className = "veiculo";
-    desc1.innerHTML = "Ano: " + ano + "\n"
-    desc2.innerHTML = "Preço: R$ " + preco + "\n"
+    imagem.src = "../img/carro 2.jpeg";
+    titulo.innerHTML = nome;
+    desc1.innerHTML = "Ano: " + ano + "\n";
+    desc2.innerHTML = "Preço: R$ " + preco + "\n";
+
+    console.log(nome,ano,preco,pos);
+
+    armazenar(nome, ano, preco, pos);
 
     sessao.appendChild(imagem);
     sessao.appendChild(titulo);
@@ -47,4 +60,64 @@ function adicionarCarro() {
     document.getElementById("nome").value = "";
     document.getElementById("ano").value = "";
     document.getElementById("preco").value = "";
+
+    modal.style.display = "none";
+}
+
+function pesquisar() {
+    let barra = document.querySelector(".barra").value;
+    let carros = document.querySelectorAll('.veiculo h2');
+
+    carros.forEach(carro => {
+        const carroText = carro.textContent.toLowerCase();
+
+        if (carroText.includes(barra)) {
+            carro.closest("section").style.display = "block";
+        } else {
+            carro.closest("section").style.display = "none";
+        }
+    });
+}
+
+function armazenar(nome, ano, preco, pos) {
+    var carro = {
+        nome: nome,
+        ano: ano,
+        preco: preco,
+    }
+
+    const stringCarro = JSON.stringify(carro);
+
+    localStorage.setItem(pos, stringCarro);
+}
+
+function carrosCadastrados() {
+
+    pos ++;
+    
+    var stringCarro = localStorage.getItem(pos);
+    
+    var carro = JSON.parse(stringCarro);
+    
+    console.log(carro);
+
+    let veiculos = document.getElementById("veiculos-grid");
+    let sessao = document.createElement("section");
+    let imagem = document.createElement("img");
+    let titulo = document.createElement("h2");
+    let desc1 = document.createElement("p");
+    let desc2 = document.createElement("p");
+
+    sessao.className = "veiculo";
+    imagem.src = "../img/carro 2.jpeg";
+    titulo.innerHTML = carro.nome;
+    desc1.innerHTML = "Ano: " + carro.ano + "\n";
+    desc2.innerHTML = "Preço: R$ " + carro.preco + "\n";
+
+    sessao.appendChild(imagem);
+    sessao.appendChild(titulo);
+    sessao.appendChild(desc1);
+    sessao.appendChild(desc2);
+
+    veiculos.appendChild(sessao);
 }
